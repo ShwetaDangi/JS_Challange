e4bd8bcacb3482d82ee7483b84cda5f35f25d0e0
ShwetaDangi <127923507+ShwetaDangi@users.noreply.github.com>
2024-07-14 18:20:26 +0530

    day2

---
+++ day02.js
@@ -0,0 +1,52 @@
+//Activity 1
+let num1 = 10;
+let num2 = 20;
+
+let add = num1 + num2;
+console.log(add);
+
+let subtract = num1 - num2;
+console.log(subtract);
+
+let multiply = num1 * num2;
+console.log(multiply);
+
+let divide = num1 / num2;
+console.log(divide);
+
+let remainder = num1 % num2;
+console.log(remainder);
+
+//Activit 2
+let number = 10;
+number += 5;
+console.log(number);
+
+let newNumber = 20;
+newNumber -= 5;
+console.log(newNumber);
+
+//Activity 3
+console.log(num1>num2);
+console.log(num1<num2);
+
+console.log(num1>=num2);
+console.log(num1<=num2);
+
+console.log(num1==num2);
+console.log(num1===num2);
+
+//Acivity 4
+let a = true;
+let b = false;
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+console.log(!b);
+
+// Actvity 5 
+
+let num = -1;
+let result = num >= 0 ? 'positive' : 'negative';
+console.log(result);
